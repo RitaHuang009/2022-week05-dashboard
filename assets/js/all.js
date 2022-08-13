b73ab6f7af394e7667ab73397a9f4a99b7e317e0
ritahuang <ritahuang@hitrust.com.tw>
2022-08-13 22:46:03 +0800

    Update 2022-08-13T14:46:01.995Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,10 @@ console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
 //# sourceMappingURL=all.js.map
